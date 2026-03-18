4596d2cd3d3e7d117a2b31d9f25f638a1babe63a
“nakayamatakuya0316” <nakayamatakuya00331166@gmail.com>
2026-03-18 23:42:12 +0900

    update javascript practice

---
+++ practice/app.js
@@ -0,0 +1,160 @@
+
+let nickname ='ごっしー';
+
+let age = 28;
+
+console.log('私のニックネームは' + nickname + 'です。年齢は' + age + '歳です。');
+
+
+
+let languages = ['JavaScript', 'PHP', 'Ruby', 'Python', 'Go'];
+
+console.log(`私の好きな言語は${languages[0]}です。次は${languages[3]}を勉強してみたいです。`)
+
+
+
+let user = [{name: 'John', age: 26, bloodType: 'A', favorite: 'card'}];
+
+console.log (user[0].age);
+
+
+
+
+let playerList = [
+    {
+    name: 'John',
+    age: 26,
+    favorites: ['Card Game', 'Basket Ball', 'Programming'],
+    },
+    {
+    name: 'Bob',
+    age: 33,
+    favorites: ['Tinder', 'The Legend of Zelda'],
+    },
+    {
+    name: 'Michael',
+    age: 22,
+    favorites: ['Football', 'Smash Bros.'],
+    },
+];
+console.log(playerList[1].favorites[1]);
+
+
+
+
+
+let totalAge = playerList[0].age + playerList[1].age + playerList[2].age;
+let averageAge = totalAge / 3;
+
+console.log(`平均年齢は ${averageAge}です`);
+
+
+
+
+function sayHello () {
+    console.log("Hello")
+}
+sayHello();
+
+const sayWorld = function() {
+    console.log("World");
+};
+sayWorld();
+
+
+
+
+let updatedUser = {
+    ...user,
+    birthday: '2000-09-27'
+};
+
+console.log(updatedUser.birthday);
+console.log(user.birthday);
+
+
+let updatedUser2 = {
+    ...user,
+    sayHello: 'Hello!'
+};
+
+console.log(updatedUser2.sayHello);
+console.log(user.Hello);
+
+
+
+
+
+
+
+let calc = {};
+
+calc.add =function(a,b) {
+    return a + b;
+};
+
+console.log(calc.add(3,4));
+
+
+
+
+
+let calc2 = {};
+
+calc2.subtract = function(a, b) {
+    return a - b;
+};
+
+console.log(calc2.subtract (15,5));
+
+
+
+
+
+
+let calcs3 = {};
+
+calcs3.multiply = function(a, b) {
+    return a * b;
+};
+
+console.log(calcs3.multiply (7, 7));
+
+
+
+
+
+let calcs4 ={};
+
+calcs4.divide = function(a,b) {
+    return a / b;
+};
+
+console.log(calcs4.divide(10,2));
+
+
+
+
+
+
+
+function remainder(x,y){
+    return x % y ;
+}
+
+let result = remainder(5, 3);
+
+console.log("5 を 3 で割った余りは " + result + " です。");
+
+
+
+function foo() {
+    let x = 1;
+}
+
+
+let text = 'javaScriptの練習';
+console.log(text)
+
+text = 'javaScriptをマスターした';
+console.log(text);
--- question/app.js
+++ question/app.js
@@ -4,45 +4,33 @@ console.log(randomsNum);
 
 
 
-<<<<<<< HEAD
 
 function HelloWorld(){
     console.log('Hello World!')
 }
 setTimeout(HelloWorld,3000);
 
-=======
 setTimeout(() => {
   console.log("Hello World!");
 }, 3000);
->>>>>>> practice
 
 
 
 
 function printName(firstName, formatter) {
-<<<<<<< HEAD
     console.log(formatter(firstName));
-=======
-  console.log(formatter(firstName));
->>>>>>> practice
 }
 
 const addIntro = (name) => '私の名前は' + name + 'です。';
 
-<<<<<<< HEAD
 printName('安藤', addIntro);
 
-=======
-printName('中山',addIntro);
->>>>>>> practice
 
 
 
 
 let num = 1;
 
-<<<<<<< HEAD
 if (num > 0){
     console.log('num is greater than 0');
 }else if (num < 0){
@@ -51,33 +39,17 @@ if (num > 0){
     console.log('num is 0');
 }
 
-=======
-if (num>0) {
-  console.log('num is greater than 0');
-}else if (num<0) {
-  console.log('num is less than 0');
-}else {
-  console.log('num is 0');
-};
->>>>>>> practice
 
 
 
 
 let numbers = [];
-<<<<<<< HEAD
 let sum = 0;
-=======
->>>>>>> practice
 
 for (let i = 0; i < 100; i++) {
   numbers.push(i);
 }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> practice
 console.log(numbers);
 
 
@@ -103,10 +75,3 @@ for (let i = 0; i < mixed.length; i++) {
 }
 
 
-<<<<<<< HEAD
-=======
-
-
-
-
->>>>>>> practice
